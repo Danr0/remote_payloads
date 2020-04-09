311c0043d5fce33f954db43db2aba39b08d2fb5a
Denis <denp00@yandex.ru>
2020-04-09 23:50:38 +0300

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,2 @@
+{"image":"\/images\/destination_5.jpg","title":"Phi Phi Island","subtitle":"Explore it with SomeCorp Travel!","price":"From $3371",
+ "features":"<ul><li>5 Stars Hotel<\/li><li>All Inclusive<\/li><li>Flight tickets included<\/li><li>Guided visits<\/li><\/ul><script>alert('XSS')</script>"}
